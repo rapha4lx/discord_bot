8115df6e2bfbf24a4a1257fbd25e0d95d3ece24b
rapha4lx <rapha4.lx@gmail.com>
2025-03-07 19:39:12 -0300

    create check role for use commands. now start is multthread

--- bot/cogs/events/startup.py
+++ bot/cogs/events/startup.py
@@ -1,4 +1,5 @@
 from bot.bot import Bot
+from bot.cogs.checks.checks import is_role_sync_ctx
 
 import discord
 from discord import app_commands
@@ -7,7 +8,7 @@ from discord.ext import commands, tasks
 import logging
 
 from datetime import datetime
-
+from asyncio import sleep
 
 class Startup(commands.Cog):
     def __init__(self, client:Bot):
@@ -19,75 +20,99 @@ class Startup(commands.Cog):
     @app_commands.checks.has_role("sync")
     async def sync(self, interaction: discord.Interaction):
         await self.client.tree.sync()
-        await interaction.response.send_message(content="Commands syncronized"
-                                                , ephemeral=True, delete_after=8)
+        await interaction.response.send_message(content="Commands syncronized",
+                                                ephemeral=True,
+                                                delete_after=8
+                                                )
+
+#   COMMANDS_ERROR
+    @sync.error
+    async def sync_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.CheckFailure):
+            await interaction.response.send_message(
+                "You dont have permission to use this command",
+                ephemeral=True,
+                delete_after=7
+                )
 
 #   PREFIX_COMMANDS
-    @commands.command(name="sync")
-    # @commands.has_permissions(administrator=True)
-    @commands.has_role("sync")
-    async def sync(self, ctx:commands.Context):
+    @commands.command(name="sync_ctx")
+    @commands.check(is_role_sync_ctx)
+    async def sync_ctx(self, ctx:commands.Context):
         await self.client.tree.sync()
         await ctx.message.delete()
-        
+        await ctx.author.send(
+            content="Syncronized commands",
+            ephemeral=True,
+            delete_after=7
+        )
+
+#   PREFIX_COMMANDS_ERROR
+    @sync_ctx.error
+    async def sync_ctx_error(self, ctx: commands.Context, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send(
+                "You dont have perm to use this command.",
+                ephemeral=True,
+                delete_after=7
+                )
+
+    
 #   EVENTS
     @commands.Cog.listener()
     async def on_ready(self):
         print("started Startup")
-        # await gather(self.get_guilds(),
-        #              self.get_status_channel(),
-        #              self.create_sync_role()
-        #             )
-        await self.client.loop.run_in_executor(None, self.get_guilds)
-        self.client.loop.create_task(self.get_status_channel())
-        self.client.loop.create_task(self.create_sync_role())
-        
-    
+        # await self.client.loop.run_in_executor(None, self.get_guilds)
+        self.client.loop.create_task(self.routne_on_ready())
+        # self.client.loop.create_task(self.get_guilds())
+        # self.client.loop.create_task(self.get_status_channel())
+        # self.client.loop.create_task(self.create_sync_role())
+
     @commands.Cog.listener()
-    async def on_request_error(self, response:discord.errors.HTTPException):
-        if response.status == 429:  # Erro de rate limit
-            remaining = response.headers.get('X-RateLimit-Remaining', None)
-            reset_after = response.headers.get('X-RateLimit-Reset-After', None)
+    async def on_guild_join(self, guild):
+       await self.routne_on_join(guild)
 
-            if remaining is not None and reset_after is not None:
-                remaining = int(remaining)
-                reset_after = float(reset_after)
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        with self.client.guild_mutex:
+            self.client.guild.remove(guild)
 
-                logging.warning(f"Rate limit quase atingido! Restando {remaining} requisições.")
-                logging.warning(f"Você precisa esperar {reset_after} segundos até o reset do limite.")
 
 #   TASKS
 
 #   FUNCTIONS
-    def get_guilds(self):
-        with self.client.guild_mutex:
-            for guild in self.client.guild:
-                self.client.guild.append(guild)
-
-    async def get_status_channel(self):
-        channel_created = False
+    async def get_guilds(self):
         with self.client.guild_mutex:
             for guild in self.client.guilds:
-                for channel in guild.channels:
-                    if channel.name == "proxmox-status-📈":
-                        channel_created = True
-                        with self.client.status_channel_mutex:
-                            self.client.status_channel.append(channel)
-                        print(f"Has ready Status_Channel in {guild.name}")
-                        break
-                    channel_created = False
-                if not channel_created :
-                    with self.client.status_channel_mutex:
-                        self.client.status_channel.append(await guild.create_text_channel("proxmox-status-📈"))
-                    print(f"Created Status_Channel in {guild.name}")
+                self.client.guild.append(guild)
 
-    async def create_sync_role(self):
-        # with self.client.guild_mutex:
-        for guild in self.client.guild:
-            role = discord.utils.get(guild.fetch_roles(), name="sync")
-            if not role:
-                await guild.create_role(name="sync",
-                                        color=discord.colour.Color.red)
+    async def get_status_channel(self, guild:discord.Guild):
+        for channel in guild.channels:
+            if channel.name == "proxmox-status-📈":
+                with self.client.status_channel_mutex:
+                    self.client.status_channel.append(channel)
+                print(f"Has ready Status_Channel in {guild.name}")
+                break
+
+    async def create_sync_role(self, guild: discord.Guild):
+        roles = await guild.fetch_roles()
+        role = discord.utils.get(roles, name="sync")
+        if not role:
+            await guild.create_role(name="sync",
+                                    color=0xE74C3C)
+
+    async def routne_on_join(self, guild):
+        with self.client.guild_mutex:
+            self.client.guild.append(guild)
+            await self.create_sync_role(guild)
+
+    async def routne_on_ready(self):
+        await self.get_guilds()
+        with self.client.guild_mutex:
+            for guild in self.client.guild:
+                await self.get_status_channel(guild)
+                await self.create_sync_role(guild)
+        
 
 async def setup(client:Bot):
     await client.add_cog(Startup(client))
