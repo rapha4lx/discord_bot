d7f1d88784980e641b0f2e96483f68c5433a20b5
rapha4lx <rapha4.lx@gmail.com>
2025-03-07 03:38:44 -0300

    created function to sync commands. now on_ready is a multi thread

--- bot/cogs/events/startup.py
+++ bot/cogs/events/startup.py
@@ -2,10 +2,11 @@ from bot.bot import Bot
 
 import discord
 from discord import app_commands
-from discord.ext import commands
-
+from discord.ext import commands, tasks
 
 import logging
+
+from datetime import datetime
 
 
 class Startup(commands.Cog):
@@ -14,11 +15,33 @@ class Startup(commands.Cog):
         super().__init__()
 
 #   COMMANDS
+    @app_commands.command(name="sync", description="sync commands")
+    @app_commands.checks.has_role("sync")
+    async def sync(self, interaction: discord.Interaction):
+        await self.client.tree.sync()
+        await interaction.response.send_message(content="Commands syncronized"
+                                                , ephemeral=True, delete_after=8)
 
+#   PREFIX_COMMANDS
+    @commands.command(name="sync")
+    # @commands.has_permissions(administrator=True)
+    @commands.has_role("sync")
+    async def sync(self, ctx:commands.Context):
+        await self.client.tree.sync()
+        await ctx.message.delete()
+        
 #   EVENTS
     @commands.Cog.listener()
     async def on_ready(self):
         print("started Startup")
+        # await gather(self.get_guilds(),
+        #              self.get_status_channel(),
+        #              self.create_sync_role()
+        #             )
+        await self.client.loop.run_in_executor(None, self.get_guilds)
+        self.client.loop.create_task(self.get_status_channel())
+        self.client.loop.create_task(self.create_sync_role())
+        
     
     @commands.Cog.listener()
     async def on_request_error(self, response:discord.errors.HTTPException):
@@ -32,9 +55,39 @@ class Startup(commands.Cog):
 
                 logging.warning(f"Rate limit quase atingido! Restando {remaining} requisições.")
                 logging.warning(f"Você precisa esperar {reset_after} segundos até o reset do limite.")
+
 #   TASKS
 
 #   FUNCTIONS
+    def get_guilds(self):
+        with self.client.guild_mutex:
+            for guild in self.client.guild:
+                self.client.guild.append(guild)
+
+    async def get_status_channel(self):
+        channel_created = False
+        with self.client.guild_mutex:
+            for guild in self.client.guilds:
+                for channel in guild.channels:
+                    if channel.name == "proxmox-status-📈":
+                        channel_created = True
+                        with self.client.status_channel_mutex:
+                            self.client.status_channel.append(channel)
+                        print(f"Has ready Status_Channel in {guild.name}")
+                        break
+                    channel_created = False
+                if not channel_created :
+                    with self.client.status_channel_mutex:
+                        self.client.status_channel.append(await guild.create_text_channel("proxmox-status-📈"))
+                    print(f"Created Status_Channel in {guild.name}")
+
+    async def create_sync_role(self):
+        # with self.client.guild_mutex:
+        for guild in self.client.guild:
+            role = discord.utils.get(guild.fetch_roles(), name="sync")
+            if not role:
+                await guild.create_role(name="sync",
+                                        color=discord.colour.Color.red)
 
 async def setup(client:Bot):
     await client.add_cog(Startup(client))
