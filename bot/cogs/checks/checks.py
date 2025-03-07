f336f9870f8b72fcec0c45166206f2a99e892342
rapha4lx <rapha4.lx@gmail.com>
2025-03-07 19:29:26 -0300

    created functions to check with user can use commands

---
+++ bot/cogs/checks/checks.py
@@ -0,0 +1,31 @@
+
+
+import discord
+from discord.ext import commands
+
+def is_role_sync_ctx(ctx:commands.Context) -> bool:
+    user:discord.Member = ctx.message.author
+    roles = user.roles
+    if not roles:
+        return False
+    role = discord.utils.get(
+        user.roles,
+        name="sync"
+        )
+    if role:
+        return True
+    return False
+
+def is_create_proxmox_status(interaction: discord.Interaction) -> bool:
+    channel = discord.utils.get(
+            interaction.guild.text_channels,
+            name="proxmox-status-📈"
+        )
+    if channel:
+        interaction.response.send_message(
+            content="Channel exist",
+            ephemeral=True,
+            delete_after=7
+            )
+        return False
+    return True
